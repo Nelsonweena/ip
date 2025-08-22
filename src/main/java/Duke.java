094e38cb651f38f33f8cb0461aae01fada35ec46
NelsonWee <Nelsonweena@gmail.com>
2025-08-22 09:23:02 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,15 +9,29 @@ public class Duke {
 
         while (true) {
             input = sc.nextLine();
+            if(input.contains(" ")){
+                String[] s = input.split(" ");
 
-            if(input.equals("bye") || input.equals("Bye")){
-                Goodbye.bye();
-                sc.close();
-                break;
-            } else if(input.equals("list") || input.equals("List")){
-                lst.displayList();
+                if(s[0].equals("mark") || s[0].equals("Mark")){
+                    lst.tickbox(Integer.parseInt(s[1]));
+
+                } else if(s[0].equals("unmark") || s[0].equals("Unmark")){
+                    lst.untickbox(Integer.parseInt(s[1]));
+                } else {
+                    lst.addToList(input);
+                }
+
             } else {
-                lst.addToList(input);
+
+                if(input.equals("bye") || input.equals("Bye")){
+                    Goodbye.bye();
+                    sc.close();
+                    break;
+                } else if(input.equals("list") || input.equals("List")){
+                    lst.displayList();
+                } else {
+                    lst.addToList(input);
+                }
             }
         }
     }
--- src/main/java/chatlist.java
+++ src/main/java/chatlist.java
@@ -2,18 +2,35 @@ import java.util.ArrayList;
 
 public class chatlist {
     private ArrayList<String> lst = new ArrayList<>();
+    private ArrayList<String> checkboxlst = new ArrayList<>();
 
     public void addToList(String input){
         lst.add(input);
+        checkboxlst.add("[ ]");
         Echo.echoback(input);
     }
 
-    public void displayList(){
-        int len = lst.size();
-
+    public void tickbox(int i){
+        checkboxlst.set((i - 1), "[X]");
         System.out.println("    ____________________________________");
-        for(int i = 0; i < len; i++){
-            System.out.println("     " + (i + 1) + ". " + lst.get(i));
+        System.out.println("     Nice! I've marked this task as done:");
+        System.out.println("     " + checkboxlst.get(i - 1) + " " + lst.get(i - 1));
+        System.out.println("    ____________________________________");
+    }
+
+    public void untickbox(int i){
+        checkboxlst.set((i - 1), "[ ]");
+        System.out.println("    ____________________________________");
+        System.out.println("     OK, I've marked this task as not done yet:");
+        System.out.println("     " + checkboxlst.get(i - 1) + " " + lst.get(i - 1));
+        System.out.println("    ____________________________________");
+    }
+
+    public void displayList(){
+        System.out.println("    ____________________________________");
+        System.out.println("     Here are the tasks in your list:");
+        for(int i = 0; i < lst.size(); i++){
+            System.out.println("     " + (i + 1) + "." + checkboxlst.get(i) + " " + lst.get(i));
         }
         System.out.println("    ____________________________________");
     }
